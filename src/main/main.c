78d02351e316d73d292052538365702283bff471
Matheus WNB <matheuswnb@WNB.wnb>
2026-01-02 00:44:33 -0300

    Update: I implemented the options the user chose, validating the choice with a switch statement.

--- src/main/main.c
+++ src/main/main.c
@@ -1,7 +1,17 @@
 #include "libs/libs.h"
 #include "menu/menu.h"
+#include "menu/opcoes.h"
 
 int main() {
     int opcao_escolhida = print_menu();
-    
+
+    switch(opcao_escolhida){
+        case 1:
+            system("clear");
+            print_edit_menu();
+            break;
+
+    }
+
+    return 0;
 }
