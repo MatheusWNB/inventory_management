3351967b6fca8d01d741226bff1029484519c11c
Matheus WNB <matheuswnb@WNB.wnb>
2026-01-02 03:19:46 -0300

    Update: The user can now type the name of the file that will be used to store the inventory items.

--- src/menu/opcoes.c
+++ src/menu/opcoes.c
@@ -2,25 +2,47 @@
 #include "menu/opcoes.h"
 #include "utils/utils.h"
 
+//Opção "1" o usuário pode editar e mostrar na tela os itens no seu estoque
 void print_edit_menu(void){
-    FILE *arquivo_estoque;
-    char nome_arquivo_estoque[20];
+    FILE *ptr_arquivo_estoque;
+    char nome_arquivo[30]; //Nome original
+    char nome_arquivo_format[50]; //Nome formatado para abrir o arquivo no programa
+    char abrir_arquivo_format[200]; //Nome formatado para abrir o .txt no sistema
+
+    clearstdin();
 
     while(true){
         printf("Digite o nome do arquivo do seu estoque: ");
         
-        clearstdin();
-        fgets(nome_arquivo_estoque, 20, stdin);
+        //Usuário digita o nome do seu arquivo
+        fgets(nome_arquivo, 20, stdin);
+        limpar_quebra_linha(nome_arquivo);
 
-        arquivo_estoque = fopen(("home/matheuswnb/c_tests/%s", nome_arquivo_estoque), "r+");
+        //Nome formatado para abrir o arquivo no programa
+        snprintf(nome_arquivo_format, sizeof(nome_arquivo_format), "%s.txt", nome_arquivo);
 
-        if (arquivo_estoque == NULL){
+        ptr_arquivo_estoque = fopen(nome_arquivo_format, "a+"); 
+        
+        fprintf(
+            ptr_arquivo_estoque,
+            "Meu nome é matheus\n"
+        );
+
+        fclose(ptr_arquivo_estoque);
+        
+        ptr_arquivo_estoque = fopen(nome_arquivo_format, "a+");
+
+        //Se a abertura do arquivo falhou, o loop continua
+        if (ptr_arquivo_estoque == NULL){
             system("clear");
             printf("Arquivo inexistente, tente novamente!\n");
             continue;
         } 
 
-        system(("open home/matheuswnb/c_tests/%s", nome_arquivo_estoque));
+        //Nome formatado para abrir o .txt no sistema
+        snprintf(abrir_arquivo_format, sizeof(nome_arquivo_format), "open %s", nome_arquivo_format);
+        //Abre o arquivo .txt
+        system(abrir_arquivo_format);
     }
 
 }
