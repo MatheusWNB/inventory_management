3676400724fb64f8fb5e8018f004ba07f6b2e169
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-01-02 21:52:21 -0300

    Add: Function that validates whether the user's answer is within a range of integers.

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -7,4 +7,7 @@ void clearstdin(void);
 //Limpa a quebra de linha do fgets
 void limpar_quebra_linha(char *nome);
 
+//Função simples para validação rápida dentro de um intervalo de números inteiros
+int validar_resposta(int min, int max, int resposta_usuario);
+
 #endif
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -11,3 +11,23 @@ void clearstdin(void){
 void limpar_quebra_linha(char *nome){
     nome[strcspn(nome, "\n")] = '\0';
 }
+
+//Retorna a validação da resposta do usuário com base em um intervalo de números inteiros
+//False = Usuário digitou uma opção fora do intervalo válido
+//True = Resposta do usuário é validada
+int validar_resposta(int a, int b, int resposta_usuario){
+    int validacao;
+
+    if (resposta_usuario < a || resposta_usuario > b){
+        system("clear");
+        printf("Por favor, digite uma opção válida!\n");
+        validacao = false;
+    }
+
+    if (resposta_usuario >= a && resposta_usuario <= b)
+        validacao = true;
+
+    return validacao;
+    
+}
+
