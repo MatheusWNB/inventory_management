f3693c0d1084157c2f05570bee17dd641c2d93ff
Matheus WNB <matheuswnb@WNB.wnb>
2026-01-04 16:38:23 -0300

    Add: 'getname' function to more securely read the names typed by the user.

--- include/utils/utils.h
+++ include/utils/utils.h
@@ -10,4 +10,7 @@ void limpar_quebra_linha(char *nome);
 //Função simples para validação rápida dentro de um intervalo de números inteiros
 int validar_resposta(int min, int max, int *resposta_usuario);
 
+//Essa função obtém o nome que o usuário digitar de maneira mais segura
+void getname(char *nome, int max_len);
+
 #endif
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -30,6 +30,48 @@ int validar_resposta(int a, int b, int *resposta_usuario){
         validacao = true;
 
     return validacao;
-    
 }
+
+//Essa função obtém o nome que o usuário digitar de maneira mais segura
+void getname(char *nome, int max_len){
+    #define BUFFER 1024 //Limite máximo de caracteres do buffer
+    char buffer_name[BUFFER]; //Buffer temporário para a variável "nome"
+
+    while(true){
+        //Obtém o nome do usuário e armazena em buffer
+        fgets(buffer_name, BUFFER, stdin); 
+        
+        //Se buffer_name não for um "enter" ele entra
+        if (buffer_name[0] != '\n'){
+            size_t len = strlen(buffer_name);
+
+            /*
+            Variável nome obtém os caracters do buffer_name
+            max_len determina a quantidade de caracteres escritos
+            */
+            snprintf(nome, max_len + 1, "%s", buffer_name);
+
+            //Se o limite do buffer for ultrapassado limpa o buffer
+            if (len > 0 && buffer_name[len - 1] != '\n')
+                clearstdin();
+
+            /*
+            Se oque o suário digitou for menor que max_len
+            limpa quebra de linha
+            */
+            if (len < (unsigned)(max_len))
+                limpar_quebra_linha(nome);
+            
+            break;
+        
+        //Loop continua se usuário não digitou nada
+        } else {
+            system("clear");
+            printf("********** DIGITE UM NOME VÁLIDO! **********\n");
+            limpar_quebra_linha(buffer_name);
+            continue;
+        }
+    }
+}
+
 
