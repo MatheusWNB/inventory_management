f3c794ec8ee21882d0cdf5ad56e70ed7399c00ae
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-01-25 01:24:58 -0300

    Update: The while loop in the set_inventory function is running according to the number of items the user wants. The items are being written to the user's file.

--- src/menu/edit_inventory.c
+++ src/menu/edit_inventory.c
@@ -2,60 +2,93 @@
 #include "utils/utils.h"
 #include "menu/opcoes.h"
 
-void edit_inventory(FILE *fp){
-    int i = 0;
+void set_inventory(FILE *fp){
+    char *name_itens = NULL;
+    char *temp_itens = NULL;
+    char *name_item = NULL;
+
+    int *len_names = NULL;
+    int *temp_len = NULL;
+
+    int quantidade_itens = 0;
     int quantidade_bytes = 0;
     int offset_bytes = 0;
-    char *ptr_itens = NULL;
-    char *nome_item = NULL;
-    int *len_names = NULL;
-    int quantidade_itens = 0;
+    int i_anterior = 0;
 
     printf("////////// EDITOR DE ESTOQUES //////////\n");
-    printf("Quais itens você gostaria de registrar no seu estoque? (max. 100 por estoque): \n");
 
-    while(i < 100){
-        printf("Item a ser registrado(max. 20 caracteres por item): ");
-        nome_item = getname(20);
-        int len_name = strlen(nome_item) + 1;
-        quantidade_bytes += len_name;
+    int contador;
+    
+    loop:
+        int i = 0;
+        printf("Quantos itens deseja adicionar?: ");
+        scanf("%d", &contador);
+        clearstdin();
 
-        char *temp = (char *)realloc(ptr_itens, quantidade_bytes + 1);
-        if(temp == NULL){
+        while(i < contador){
+            printf("Item a ser registrado(max. 20 caracteres por item): ");
+            name_item = getname(20);
+            int len_name = strlen(name_item) + 1;
+            quantidade_bytes += len_name;
+
+            temp_itens = (char *)realloc(name_itens, quantidade_bytes + 1);
+            if(temp_itens == NULL){
+                system("clear");
+                printf("********** REGISTRO DE ITEM FALHOU **********\n");
+                free(temp_itens);
+                break;
+            }
+
+            //Copia o nome do item para o começo do novo endereço realocado
+            name_itens = temp_itens;
+            strcpy(name_itens + offset_bytes, name_item);
+            quantidade_itens++;
+
+            int *temp_len = realloc(len_names, quantidade_itens * sizeof(int));
+            if(temp_len == NULL){
+                system("clear");
+                printf("********** REGISTRO DE LEN FALHOU **********\n");
+                free(temp_len);
+                break;
+            }
+
+            //Armazena o offset para o ínicio de cada item
+            len_names = temp_len;
+            len_names[i_anterior] = offset_bytes;
+
             system("clear");
-            printf("********** REGISTRO DE ITEM FALHOU **********\n");
-            free(temp);
-            break;
+
+            printf("////////// SEUS ITENS //////////\n");
+            for(int i1 = 0; i1 < quantidade_itens; i1++){
+                printf("%d -> %s\n", i1 + 1, name_itens + len_names[i1]);
+                printf("--------\n");
+            }
+
+            //Offset para o ínicio do próximo item
+            offset_bytes = quantidade_bytes + 1;
+            i++;
+            i_anterior++;
         }
 
-        //Copia o nome do item para o começo do novo endereço realocado
-        ptr_itens = temp;
-        strcpy(ptr_itens + offset_bytes, nome_item);
-        quantidade_itens++;
-
-        int *temp_array = realloc(len_names, quantidade_itens * sizeof(int));
-        if(temp_array == NULL){
-            system("clear");
-            printf("********** REGISTRO DE LEN FALHOU **********\n");
-            free(temp_array);
-            break;
-        }
-
-        //Armazena o offset para o ínicio de cada item
-        len_names = temp_array;
-        len_names[i] = offset_bytes;
-
-        system("clear");
-
-        printf("////////// SEUS ITENS //////////\n");
+    printf("Deseja adicionar mais itens?(s/n): ");
+    char *resposta = getname(1);
+    
+    if(strncmp(resposta, "s", 1) == 0){
+        goto loop;
+    } else if(strncmp(resposta, "n", 1) == 0){
+        fprintf(fp, "////////// SEUS ITENS //////////\n");
         for(int i1 = 0; i1 < quantidade_itens; i1++){
-            printf("%d -> %s\n", i1 + 1, ptr_itens + len_names[i1]);
-            printf("--------\n");
+            fprintf(fp,
+                "%d -> %s\n"
+                "---------\n", i1 + 1, name_itens + len_names[i1]
+            );
         }
 
-        //Offset para o ínicio do próximo item
-        offset_bytes = quantidade_bytes + 1;
-        i++;
+        free(len_names);
+        free(name_item);
+        free(name_itens);
+
+        fclose(fp);
     }
 }
 
