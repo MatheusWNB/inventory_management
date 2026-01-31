413cd1af544c076eabba0e0304cf46d16152864c
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-01-31 04:15:04 -0300

    update: Organizing the code and adding a loop system to set_inventory

--- src/menu/edit_inventory.c
+++ src/menu/edit_inventory.c
@@ -15,80 +15,139 @@ void set_inventory(FILE *fp){
     int offset_bytes = 0;
     int i_anterior = 0;
 
-    printf("////////// EDITOR DE ESTOQUES //////////\n");
+    while(true){
+        printf("////////// EDITOR DE ESTOQUES //////////\n");
+        printf(
+            "Qual operação você deseja realizar?\n"
+            "(1)Adicionar itens ao meu estoque\n"
+            "(2)Visualizar e editar meu estoque\n"
+            "(3)Fechar editor\n"
+        );
 
-    int contador;
-    
-    loop:
-        int i = 0;
-        printf("Quantos itens deseja adicionar?: ");
-        scanf("%d", &contador);
+        printf("Sua escolha: ");
+        int escolha;
+        scanf("%d", &escolha);
+        validar_resposta(1, 2, &escolha);
         clearstdin();
 
-        while(i < contador){
-            printf("Item a ser registrado(max. 20 caracteres por item): ");
-            name_item = getname(20);
-            int len_name = strlen(name_item) + 1;
-            quantidade_bytes += len_name;
+        if(escolha == 1){
+            int contador;
+            loop:
+                int i = 0;
+                printf("Quantos itens deseja adicionar?: ");
+                scanf("%d", &contador);
+                clearstdin();
 
-            temp_itens = (char *)realloc(name_itens, quantidade_bytes + 1);
-            if(temp_itens == NULL){
-                system("clear");
-                printf("********** REGISTRO DE ITEM FALHOU **********\n");
-                free(temp_itens);
-                break;
+                while(i < contador){
+                    printf("Item a ser registrado(max. 20 caracteres por item): ");
+                    name_item = getname(20);
+                    int len_name = strlen(name_item) + 1;
+                    quantidade_bytes += len_name;
+
+                    temp_itens = (char *)realloc(name_itens, quantidade_bytes + 1);
+                    if(temp_itens == NULL){
+                        system("clear");
+                        printf("********** REGISTRO DE ITEM FALHOU **********\n");
+                        free(temp_itens);
+                        break;
+                    }
+
+                    //Copia o nome do item para o começo do novo endereço realocado
+                    name_itens = temp_itens;
+                    strcpy(name_itens + offset_bytes, name_item);
+                    quantidade_itens++;
+
+                    int *temp_len = realloc(len_names, quantidade_itens * sizeof(int));
+                    if(temp_len == NULL){
+                        system("clear");
+                        printf("********** REGISTRO DE LEN FALHOU **********\n");
+                        free(temp_len);
+                        break;
+                    }
+
+                    //Armazena o offset para o ínicio de cada item
+                    len_names = temp_len;
+                    len_names[i_anterior] = offset_bytes;
+
+                    system("clear");
+
+                    printf("////////// SEUS ITENS //////////\n");
+                    for(int i1 = 0; i1 < quantidade_itens; i1++){
+                        printf("%d -> %s\n", i1 + 1, name_itens + len_names[i1]);
+                        printf("--------\n");
+                    }
+
+                    //Offset para o ínicio do próximo item
+                    offset_bytes = quantidade_bytes + 1;
+                    i++;
+                    i_anterior++;
+                }
+
+            printf("Deseja adicionar mais itens?(s/n): ");
+            char *resposta = getname(1);
+            
+            if(strncmp(resposta, "s", 1) == 0){
+                goto loop;
+
+            } else if(strncmp(resposta, "n", 1) == 0){
+                fprintf(fp, "////////// SEUS ITENS //////////\n");
+                for(int i1 = 0; i1 < quantidade_itens; i1++){
+                    fprintf(fp,
+                        "%d -> %s\n"
+                        "---------\n", i1 + 1, name_itens + len_names[i1]
+                    );
+                }
             }
 
-            //Copia o nome do item para o começo do novo endereço realocado
-            name_itens = temp_itens;
-            strcpy(name_itens + offset_bytes, name_item);
-            quantidade_itens++;
+            continue;
 
-            int *temp_len = realloc(len_names, quantidade_itens * sizeof(int));
-            if(temp_len == NULL){
-                system("clear");
-                printf("********** REGISTRO DE LEN FALHOU **********\n");
-                free(temp_len);
-                break;
+        }else if(escolha == 2){
+            printf("////////// VISUALIZAR E EDITAR ESTOQUE //////////\n");
+            int any;
+            
+            if(name_itens == NULL){
+                printf("Seu estoque está vazio, deseja adicionar algum item?(s/n)\n");
+                while(true){
+                    char *resposta = getname(1);
+
+                    if(strncmp(resposta, "s", 1) == 0){
+                        any = 0;
+                        goto loop;
+
+                    }else if(strncmp(resposta, "n", 1) == 0){
+                        any = 1;
+                        break;
+                    }else{
+                        printf("********** DIGITE UMA RESPOSTA VÁLIDA (s/n) **********");
+                        continue;
+                    }
+                }
             }
 
-            //Armazena o offset para o ínicio de cada item
-            len_names = temp_len;
-            len_names[i_anterior] = offset_bytes;
+            if(any == 1)
+                continue;
 
             system("clear");
-
             printf("////////// SEUS ITENS //////////\n");
+
             for(int i1 = 0; i1 < quantidade_itens; i1++){
                 printf("%d -> %s\n", i1 + 1, name_itens + len_names[i1]);
                 printf("--------\n");
             }
+            getchar();
+                
+        }else if(escolha == 3){
+            system("clear");
+            printf("********** FECHANDO O EDITOR DE ESTOQUE **********\n");
 
-            //Offset para o ínicio do próximo item
-            offset_bytes = quantidade_bytes + 1;
-            i++;
-            i_anterior++;
+            free(len_names);
+            free(name_item);
+            free(name_itens);
+
+            fclose(fp);
+
+            break;
         }
-
-    printf("Deseja adicionar mais itens?(s/n): ");
-    char *resposta = getname(1);
-    
-    if(strncmp(resposta, "s", 1) == 0){
-        goto loop;
-    } else if(strncmp(resposta, "n", 1) == 0){
-        fprintf(fp, "////////// SEUS ITENS //////////\n");
-        for(int i1 = 0; i1 < quantidade_itens; i1++){
-            fprintf(fp,
-                "%d -> %s\n"
-                "---------\n", i1 + 1, name_itens + len_names[i1]
-            );
-        }
-
-        free(len_names);
-        free(name_item);
-        free(name_itens);
-
-        fclose(fp);
     }
 }
 
--- src/menu/opcoes.c
+++ src/menu/opcoes.c
@@ -7,17 +7,14 @@
 int print_inventory(int *opcao_escolhida){
     FILE *ptr_arquivo_estoque = NULL;
     char *nome_arquivo = NULL;//Nome original
-    int len_nome_arquivo;
 
     char *nome_arquivo_format = NULL; //Nome formatado para abrir o arquivo no programa
     char *abrir_arquivo_format = NULL; //Nome formatado para abrir o .txt no sistema
 
-    char format_txt[7] = "%s.txt";
+    char format_txt[7] = "%s.bin";
     int len_format_txt = strlen(format_txt);
     char format_open_txt[8] = "open %s";
     int len_format_open = strlen(format_open_txt);
-
-    int total_len;
 
     while(true){
         printf("////////// GERENCIAR ESTOQUE ////////// \n");
@@ -25,14 +22,16 @@ int print_inventory(int *opcao_escolhida){
         
         //Usuário digita o nome do seu arquivo
         nome_arquivo = getname(15);
-        len_nome_arquivo = strlen(nome_arquivo);
+        int len_nome_arquivo = strlen(nome_arquivo);
+
+        int total_len = len_nome_arquivo + len_format_txt;
         
         nome_arquivo_format = alloc_mem(total_len);
 
         //Formata o nome do arquivo que o usuário digitou para .txt e tenta abrir o arquivo
         snprintf(nome_arquivo_format, total_len + 1, format_txt, nome_arquivo);
-        ptr_arquivo_estoque = fopen(nome_arquivo_format, "r+"); 
-        
+        ptr_arquivo_estoque = fopen(nome_arquivo_format, "r+b");
+
         //Se a abertura do arquivo falhou, o usuário decide oque vai fazer
         if (ptr_arquivo_estoque == NULL){
             system("clear");
@@ -79,6 +78,9 @@ int print_inventory(int *opcao_escolhida){
 
         snprintf(abrir_arquivo_format, total_len, format_open_txt, nome_arquivo_format);
         system(abrir_arquivo_format);
+        
+        *opcao_escolhida = 0;
+        return *opcao_escolhida;
     }
 }
 
@@ -88,7 +90,7 @@ int register_inventory(int *opcao_escolhida){
     char *nome_arquivo = NULL; //Nome original
     char *nome_arquivo_format = NULL; //Nome formatado para abrir o arquivo no programa
 
-    char format_txt[7] = "%s.txt";
+    char format_txt[7] = "%s.bin";
     int len_format_txt = strlen(format_txt);
 
     int total_len;
@@ -106,7 +108,7 @@ int register_inventory(int *opcao_escolhida){
 
         //Formata o nome do arquivo que o usuário digitou para .txt e tenta abrir o arquivo
         snprintf(nome_arquivo_format, total_len + 1, format_txt, nome_arquivo);
-        ptr_arquivo_estoque = fopen(nome_arquivo_format, "a+");
+        ptr_arquivo_estoque = fopen(nome_arquivo_format, "a+b");
 
         //Se a criação do arquivo falhou, o usuário decide oque vai fazer
         if (ptr_arquivo_estoque == NULL){
@@ -145,7 +147,7 @@ int register_inventory(int *opcao_escolhida){
         printf("O seu arquivo foi criado com sucesso!\n");
         printf("Aperte 'ENTER' para ser redirecionado ao menu de edição de estoque: ");
         getchar();
-    
+
         fclose(ptr_arquivo_estoque);
         free(nome_arquivo);
     
