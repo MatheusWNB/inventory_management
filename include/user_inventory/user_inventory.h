a363657d0741ff9166baf4cd51a3f699c8613d86
Matheus Brito <matheuswnb@kali.kaliwnb>
2026-01-31 21:01:35 -0300

    Add: encoding of functions related to user files

---
+++ include/user_inventory/user_inventory.h
@@ -0,0 +1,23 @@
+#ifndef USER_INVENTORY_H
+#define USER_INVENTORY_H
+
+#include "libs/libs.h"
+#include "utils/utils.h"
+
+typedef struct {
+    char *username;
+    int *id_items;
+    FILE *user_inventories;
+    FILE *log_inventories;
+}user_inventories;
+
+void info_inventory(user_inventories *arg, char *name, int *ids, 
+    FILE *inventories, FILE *logs);
+
+void verify_inventory(FILE *fp, int *validate);
+
+void set_inventory(FILE *fp, char *username, char *items, 
+    int *total_items, int *offset_items);
+
+#endif
+
---
+++ src/user_inventory/user_inventory.c
@@ -0,0 +1,44 @@
+#include "user_inventory/user_inventory.h"
+
+void info_inventory(user_inventories *arg, char *name, int *ids, 
+                    FILE *inventories, FILE *logs)
+{
+    arg->username = name;
+    arg->id_items = ids;
+    arg->user_inventories = inventories;
+    arg->log_inventories = logs;
+}
+                        
+                
+void verify_inventory(FILE *fp, int *validate){
+    fseek(fp, 0, SEEK_END);
+    int is_empty = ftell(fp);
+
+    if(is_empty == 0)
+        *validate = false;
+    else if(is_empty > 0)
+        *validate = true;
+}
+
+void set_inventory(FILE *fp, char *username, char *items, 
+                        int *total_items, int *offset_items)
+{      
+    int validate;
+    verify_inventory(fp, &validate);
+
+    if(validate == 0){
+        int len = strlen(username);
+        fwrite(username, sizeof(char), len, fp);
+    }
+
+    int len_item;
+    int total_bytes;
+    for(int i = 0; i < *total_items; i++){
+        len_item = strlen(items + offset_items[i]);
+        fwrite(i, sizeof(int), 1, fp);
+        fwrite(offset_items[i], sizeof(int), 1, fp);
+        fwrite(items + offset_items[i], sizeof(char), len_item, fp);
+    }
+
+    total_bytes = ftell(fp);
+}
