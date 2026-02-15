65fe5fd66d3e09aed9b0ef7cd0e9a3673b75c06d
Matheus Weinfurter <matheusweinfurter@gmail.com>
2026-02-15 19:47:23 -0300

    These functions do not require a pointer to the option.

--- include/menu/opcoes.h
+++ include/menu/opcoes.h
@@ -1,8 +1,8 @@
 #ifndef OPCOES_H
 #define OPCOES_H
 
-int print_inventory(int *opcao_escolhida, char *nome);
+int print_inventory(char *nome);
 
-int register_inventory(int *opcao_escolhida);
+int register_inventory();
 
 #endif
